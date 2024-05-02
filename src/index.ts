fa141ddbfdfd5e97058595893f614223ef8a1529
Ronaldo Rua <ronaldorua86@gmail.com>
2024-05-01 22:33:50 -0400

    cors origins

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,9 @@ import routerStartups from "./routers/startups.router";
 import routerExpositores from "./routers/expositores.router";
 import routerAuth from "./routers/auth.router";
 import routerSugerencias from "./routers/sugerencias.router";
-import Personas from "./models/personas";
-import { resultadosStartups } from "./controllers/startups.controller";
-import Startups from "./models/startups";
-import { formatInTimeZone } from "date-fns-tz";
-const cors = require('cors');
+var cors = require('cors');
 require('dotenv').config();
-const app:Express = express();
-// const cron = require('node-cron');
+var app:Express = express();
 
 const port = Number(process.env.PORT) || 3000;
 // Middleware
@@ -21,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   origin: 'https://tarija.dialoga.tja.dev.404.codes',
+  optionsSuccessStatus: 200,
   preflightContinue: true
+
 }));
 
 // Routers
@@ -46,33 +43,3 @@ async function main() {
 }
 
 main();
-
-// cron.schedule('0 0 25 4 *', async function () {
-//   await Personas.update({encuestado:false},{where:{encuestado:true}});
-  
-// },undefined,true,"America/La_Paz");
-
-// cron.schedule('0 0 0 25 4 *', async function () {
-// try {
-  
-//   let resultados = await resultadosStartups();
-//   while (resultados.length > 2) {
-//     resultados.pop();
-//   }
-//   for(const r of resultados){
-//     let startup: Startups = new Startups();
-//     startup.descripcion = r.descripcion;
-//     let fecha = new Date();
-//     const fechaStr = formatInTimeZone(fecha, 'America/La_Paz', 'yyyy-MM-dd')
-//     startup.fecha = formatInTimeZone(fechaStr, 'America/La_Paz', 'yyyy-MM-dd') as any;
-//     startup.foto = r.foto;
-//     startup.nombre = r.nombre;
-//     let body = {foto:startup.foto, nombre:startup.nombre, descripcion:startup.descripcion} as any;
-//     await Startups.create(body);
-//   }
-
-// } catch (error: any) {
-//     console.error('Error al agregar startups:', error.message);
-// }
-
-// },undefined,true,"America/La_Paz");
